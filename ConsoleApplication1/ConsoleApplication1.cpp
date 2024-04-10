02e9b5ddcfa4e6f1bb0197746c8c8f1972895c47
ouxx2 <n1247@bk.ru>
2024-04-10 11:27:12 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	double hl, hm, V;
+	V = 1,27 * (Q / pow(d,2));
+	hl = D * (L / d) * (pow(V,2)/19.6);
+	hm = 0.1 * hl;
+	H = Hg + hl + hm + Hs;
+}
